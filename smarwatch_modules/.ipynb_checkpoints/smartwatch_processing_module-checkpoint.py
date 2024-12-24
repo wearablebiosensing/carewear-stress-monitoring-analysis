6c7acf70a6096bba841ac48db42e6165bbcc3001
sadhushehjar <45221445+sadhushehjar@users.noreply.github.com>
2024-12-24 15:55:42 -0500

    Care wear belt code update.

--- smarwatch_modules/.ipynb_checkpoints/smartwatch_processing_module-checkpoint.py
+++ smarwatch_modules/.ipynb_checkpoints/smartwatch_processing_module-checkpoint.py
@@ -289,10 +289,22 @@ def plot_sample_rate(sample_rate_dict,pid):
     # Extracting keys and values for plotting
     activities = list(sample_rate_dict.keys())
     durations = list(sample_rate_dict.values())
+        # Extracting keys and values
+    activities = list(sample_rate_dict.keys())
+    durations = list(sample_rate_dict.values())
+    custom_order = ["Rest_1","Prepare_Speech","Give_Speech","Rest_2","Mental_Math","Rest_3","Stationary_Bike_Legs","Stationary_Bike_Hand"]
 
+    # Reorder based on custom_order if provided
+    if custom_order:
+        ordered_activities = [activity for activity in custom_order if activity in activities]
+        ordered_durations = [sample_rate_dict[activity] for activity in ordered_activities]
+    else:
+        # Default to the original order if no custom order is provided
+        ordered_activities = activities
+        ordered_durations = durations
     # Creating the bar plot
     fig = go.Figure([
-        go.Bar(x=activities, y=durations, marker_color='blue',text=durations,textposition='inside')
+        go.Bar(x=ordered_activities, y=ordered_durations, marker_color='blue',text=durations,textposition='inside')
     ])
 
     # Adding titles and improving aesthetics
@@ -385,7 +397,7 @@ def find_missing_samples(df, timestamp_column):
     # Find missing seconds
     missing_seconds = [time for time in full_range if time not in existing_seconds]
     
-    return len(full_range),len(existing_seconds)
+    return len(full_range),df.shape[0]#len(existing_seconds)
 # Companion run function to get sample rate of each activity.
 # Input: A pandas df with activty column.
 # Returns a dictioanry with keys as sctivity and value as sample rate.
@@ -402,13 +414,26 @@ here -- [300, 298] => [expected_samples, recieved_samples].
  'Mental_Math': [300, 298]}
 '''
 def get_expected_recieved_samples(hr_df):
-    
     activity_list = hr_df["activity"].unique()
     # ['Stationary_Bike_Legs', 'Rest_3', 'Rest_1', 'Rest_2',
     #    'Give_Speech', 'Prepare_Speech', 'Stationary_Bike_Hand',
     #    'Mental_Math']
+    sample_rate_actual = get_activity_based_sr(hr_df)
     missing_samples_dict = {}
     for activity in activity_list:
+        print("get_expected_recieved_samples()/: activity:=========",activity)
+        # Get start and end times of each activity.
+#         start_time = hr_df[hr_df["activity"]==activity].iloc[0]["Timestamp"] #df['watch_timestamp'].min()
+#         print("get_expected_recieved_samples(): hr_df \n",hr_df[hr_df["activity"]==activity].tail())
+#         end_time = hr_df[hr_df["activity"]==activity].iloc[-1]["Timestamp"] #df['watch_timestamp'].max()
+        
+#         print("get_expected_recieved_samples(): start_time,end_time: ",start_time,end_time)
+#         print("get_expected_recieved_samples(): sample Rate activity",sample_rate_actual[activity])
+#         # Calculate the difference in seconds
+#         elapsed_seconds = (end_time - start_time).total_seconds()
+#         expected_num_samples = int(elapsed_seconds*sample_rate_actual[activity])
+#         print("get_expected_recieved_samples(): EXPECTED SAMPLES =  #Seconds * SR",expected_num_samples)
+    
         #print("activity ===================== ",activity)
         #print("df activity",hr_df[hr_df["activity"]==activity])
         len_expected_samples,len_recieved_samples = find_missing_samples(hr_df[hr_df["activity"]==activity], "Timestamp") #find_missing_samples(hr_df, activity) #calculate_sample_rate(hr_df,activity)
@@ -427,20 +452,46 @@ def get_expected_recieved_samples(hr_df):
  'Mental_Math': [300, 299]}
 
 '''
-def plot_expected_recieved_samples(missing_samples_dict):
+def plot_expected_recieved_samples(missing_samples_dict,pid):
     # Create the grouped bar plot using Plotly
     fig = go.Figure()
-
+    
     # Extract keys and values
     keys = list(missing_samples_dict.keys())
     values = np.array(list(missing_samples_dict.values()))
+    custom_order = ["Rest_1","Prepare_Speech","Give_Speech","Rest_2","Mental_Math","Rest_3","Stationary_Bike_Legs","Stationary_Bike_Hand"]
+
+
+#     # Reorder keys and values based on custom_order if provided
+#     if custom_order:
+#         ordered_keys = [key for key in custom_order if key in keys]
+#         ordered_values = [missing_samples_dict[key] for key in ordered_keys]
+        
+#     else:
+#         # Default to the original order if no custom order is provided
+#         ordered_keys = keys
+#         ordered_values = values
+#         # Ensure all activities in custom_order are present in the dictionary, with defaults of [0, 0]
+#     complete_samples_dict = {key: missing_samples_dict.get(key, [0, 0]) for key in custom_order}
+
+#     # Separate the values for red and blue bars
+#     ordered_values = np.array(ordered_values)
+    # Ensure all activities in custom_order are present in the dictionary, with defaults of [0, 0]
+    complete_samples_dict = {key: missing_samples_dict.get(key, [0, 0]) for key in custom_order}
+
+    # Extract ordered keys and values
+    ordered_keys = list(complete_samples_dict.keys())
+    ordered_values = np.array(list(complete_samples_dict.values()))
+
+    red_values = ordered_values[:, 0]
+    blue_values = ordered_values[:, 1]
 
     # Separate the values for red and blue bars
     red_values = values[:, 0]
     blue_values = values[:, 1]
     # Add red bars for index 0 values
     fig.add_trace(go.Bar(
-        x=keys,
+        x=ordered_keys,
         y=red_values,
         text=red_values,textposition='inside',
         name='Expected number of samples',
@@ -449,7 +500,7 @@ def plot_expected_recieved_samples(missing_samples_dict):
 
     # Add blue bars for index 1 values
     fig.add_trace(go.Bar(
-        x=keys,
+        x=ordered_keys,
         y=blue_values,
         text=blue_values,textposition='inside',
         name='Recieved number of samples',
@@ -459,7 +510,7 @@ def plot_expected_recieved_samples(missing_samples_dict):
     # Update layout for better visualization
     fig.update_layout(
             title={
-                    'text': "Number of samples",
+                    'text': pid + ": Number of samples",
                     'x': 0.4,  # Centers the title
                     'xanchor': 'center',
                     'yanchor': 'top'
@@ -473,4 +524,90 @@ def plot_expected_recieved_samples(missing_samples_dict):
     return fig
     # Show plot
     # fig.show()
+def participant_based_fig(hr_df_pid,participant_folder):
+    print("participant_based_fig: ",participant_folder)
+    missing_samples_dict = get_expected_recieved_samples(hr_df_pid)
+    sample_rate_dict = get_activity_based_sr(hr_df_pid)
     
+    fig1 = plot_expected_recieved_samples(missing_samples_dict,participant_folder)
+    fig2 = plot_sample_rate(sample_rate_dict,participant_folder)
+    
+    # Create a subplot layout with one row and two columns
+    combined_fig = make_subplots(rows=1, cols=2, subplot_titles=(participant_folder + ": Expected vs Received Samples", participant_folder+ ": Sample Rate"))
+
+    # Add traces from fig1 to the first subplot
+    for trace in fig1.data:
+        combined_fig.add_trace(trace, row=1, col=1)
+
+    # Add traces from fig2 to the second subplot
+    for trace in fig2.data:
+        combined_fig.add_trace(trace, row=1, col=2)
+
+    # Update layout for aesthetics
+    combined_fig.update_layout(
+        title="Participant-Based Analysis",
+        showlegend=True,
+        height=600,  # Adjust height as needed
+        width=1500,  # Adjust width as needed
+    )
+
+    # Return the combined figure
+    return combined_fig
+
+    
+# Plot raw heart rate data with activity annotations.
+# Input - Particiapnt based heart rate df with all the actiivties.
+def plot_hr_raw_data(df,pid):
+    # Get the first ouxxurence of the task
+    rest_1_first_occurrence_index_df = (df['activity'] == 'Rest_1').idxmax() #df[df['activity'] == 'Rest_1'].index[0]
+    prepare_speech_first_occurrence_index_df = (df['activity'] == 'Prepare_Speech').idxmax()
+    give_speech_first_occurrence_index_df = (df['activity'] == 'Give_Speech').idxmax()
+    rest_2_first_occurrence_index_df = (df['activity'] == 'Rest_2').idxmax()  #df[df['activity'] == 'Rest_3'].index[0]
+
+    mental_math_first_occurrence_index_df = (df['activity'] == 'Mental_Math').idxmax()
+    rest_3_first_occurrence_index_df = (df['activity'] == 'Rest_3').idxmax()  #df[df['activity'] == 'Prepare_Speech'].index[0]
+    stationary_bike_hand_first_occurrence_index_df = (df['activity'] == 'Stationary_Bike_Hand').idxmax()
+    bike_legs_first_occurrence_index_df = (df['activity'] == 'Stationary_Bike_Legs').idxmax() #df[df['activity'] == 'Stationary_Bike_Legs'].index[0]
+    print("bike_legs_first_occurrence_index_df: ",bike_legs_first_occurrence_index_df)
+    print("stationary_bike_hand_first_occurrence_index_df: ",stationary_bike_hand_first_occurrence_index_df)
+    fig1 = px.line(df, y="heart_rate_float",line_shape="linear", color_discrete_sequence=['#0072BD'],template="plotly_white")
+
+    fig1.add_vline(x=rest_1_first_occurrence_index_df,  annotation_text="Rest1", line_width=3, line_dash="solid", line_color="grey")
+    fig1.add_vline(x=prepare_speech_first_occurrence_index_df, annotation_text="PS",line_width=3, line_dash="solid", line_color="grey")
+    fig1.add_vline(x=give_speech_first_occurrence_index_df,annotation_text="Give Speech", line_width=3, line_dash="solid", line_color="grey")
+    fig1.add_vline(x=rest_2_first_occurrence_index_df,annotation_text="Rest 2", line_width=3,line_dash="solid", line_color="grey")
+    fig1.add_vline(x=mental_math_first_occurrence_index_df, annotation_text="Mental Math",line_width=3,line_dash="solid", line_color="grey")
+    fig1.add_vline(x=rest_3_first_occurrence_index_df,annotation_text="Rest 3", line_width=3, line_dash="solid", line_color="grey")
+    fig1.add_vline(x=stationary_bike_hand_first_occurrence_index_df,annotation_text="Bike Hand", line_width=3, line_dash="solid", line_color="grey")
+    fig1.add_vline(x=bike_legs_first_occurrence_index_df, annotation_text="Bike Legs",line_width=3, line_dash="solid", line_color="grey")
+    # fig1.update_yaxes(title_text='Heart Rate (bpm)', range=[40, 180], tickmode='linear', dtick=10)
+    fig1.update_yaxes(title_text='Heart Rate (bpm)', range=[-10, 180], tickmode='linear', dtick=10)
+
+    # Update layout to adjust title position and distance from plot
+    fig1.update_layout(
+            xaxis=dict(
+            title="Time in seconds",               # Title for the x-axis
+            showline=True,              # Show x-axis line
+            linecolor='black',          # Color of x-axis line
+            linewidth=2,                # Width of x-axis line
+           # mirror=True                 # Mirror the axis line (to extend it across the top of the plot)
+        ),
+        yaxis=dict(
+            title="Heart Rate (bpm)",    # Title for the y-axis
+            showline=True,               # Show y-axis line
+            linecolor='black',           # Color of y-axis line
+            linewidth=2,                 # Width of y-axis line
+           # mirror=True                  # Mirror the axis line (to extend it across the right side of the plot)
+        ),
+        title={
+            'text':pid + " : Smart watch HR data in bpm",
+            'x': 0.5,               # Center the title horizontally
+            'xanchor': 'center',
+            'y': 0.95,              # Adjust vertical position (closer to plot)
+            'yanchor': 'top',        # Set anchor point to top
+            'pad': {'b': 10}         # Adjust padding (decrease distance)
+        }
+    )
+    # Show the plot
+    # fig1.show()
+    return fig1
